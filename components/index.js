a98b45b2094332b28c2a346c745b1b1923a77a0c
JASH-PATEL-6566 <jashpatel6566@gmail.com>
2023-07-17 22:46:51 +0530

    version 1.0

---
+++ components/index.js
@@ -0,0 +1,29 @@
+import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn.jsx";
+
+// home screen
+import Welcome from "./home/welcome/Welcome.jsx";
+import Nearbyjobs from "./home/nearby/Nearbyjobs.jsx";
+import Popularjobs from "./home/popular/Popularjobs.jsx";
+
+// job details screen
+import Company from "./jobdetails/company/Company.jsx";
+import { default as JobTabs } from "./jobdetails/tabs/Tabs";
+import { default as JobAbout } from "./jobdetails/about/About";
+import { default as JobFooter } from "./jobdetails/footer/Footer";
+import Specifics from "./jobdetails/specifics/Specifics.jsx";
+
+// common
+import NearbyJobCard from "./common/cards/nearby/NearbyJobCard";
+
+export {
+  ScreenHeaderBtn,
+  Welcome,
+  Nearbyjobs,
+  Popularjobs,
+  Company,
+  JobTabs,
+  JobAbout,
+  JobFooter,
+  Specifics,
+  NearbyJobCard
+};
